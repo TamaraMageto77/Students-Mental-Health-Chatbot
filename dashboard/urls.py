f6f66601401eb84e5614dc5f51fad18bce7737fa
owayo-cloud <owayopaul@gmail.com>
2024-10-31 15:29:22 +0300

    added alerts url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, accounts_list,account_upgrade, account_detail, account_delete, account_edit
+from .views import dashboard, accounts_list,account_upgrade, account_detail, account_delete, account_edit, alerts
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('accounts/', accounts_list, name='accounts'),
+    path('alert/', alerts, name='alert'),
     path('accounts/<int:id>/', account_detail, name='account_detail'),
     path('accounts/<int:id>/delete', account_delete, name='account_delete'),
     path('accounts/<int:id>/edit', account_edit, name='account_edit'),
